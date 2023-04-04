381201cd7da13d19bb7c95e21bd32a6b541db28a
Пользователь <whiskygogo@mail.ru>
2023-04-04 21:40:54 +0300

    Homework 2.7 done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Map<Integer, String> employeeBook = new HashMap<>(Map.of());
+        employeeBook.put (1, "David Beckham");
+        employeeBook.put (2, "Lionel Messi");
+        employeeBook.put (3, "Erling Haaland");
+        employeeBook.put (4, "Steven Gerrard");
+        employeeBook.put (5, "Zinedine Zidane");
+
+        System.out.println(employeeBook.get(1));
+        employeeBook.remove(2);
+        System.out.println(employeeBook);
     }
 }
